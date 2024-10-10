b063a484615dfa6943a4e4c12ef0839a0725bdf7
Hossein Zare <zarehossein.hz1997@gmail.com>
2024-10-10 19:19:42 +0330

    Update.

--- src/init.php
+++ src/init.php
@@ -8,24 +8,26 @@ if (request()->routeIs('client.product.index')) {
     pluginRepository()->addAction(
         hookName: 'head',
         callback: function () {
-            $product = \View::getShared()['product'];
+            $product = \View::getShared()['product'] ?? null;
             
-            $html = '
+            if ($product) {
+                $html = '
             
-                <!-- Torob Tags -->
-                <meta name="product_id" content="'. $product->id .'">
-                <meta name="product_name" content="'. e($product->title) .'">
-                <meta name="product_price" content="'. $product->final_price .'">
-                <meta name="product_old_price" content="'. $product->price .'">
-                <meta name="availability" content="'. ($product->is_quantity_unlimited || $product->quantity > 0 ? 'instock' : 'outofstock') .'">
-            
-            ';
+                    <!-- Torob Tags -->
+                    <meta name="product_id" content="'. $product->id .'">
+                    <meta name="product_name" content="'. e($product->title) .'">
+                    <meta name="product_price" content="'. $product->final_price .'">
+                    <meta name="product_old_price" content="'. $product->price .'">
+                    <meta name="availability" content="'. ($product->is_quantity_unlimited || $product->quantity > 0 ? 'instock' : 'outofstock') .'">
+                
+                ';
 
-            if ($product->image) {
-                $html .= '<meta property="og:image" content="'. $product->image->url['medium'] .'">';
+                if ($product->image) {
+                    $html .= '<meta property="og:image" content="'. $product->image->url['medium'] .'">';
+                }
+
+                return $html;
             }
-
-            return $html;
         },
     );
 }
